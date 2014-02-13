2794af965965b7bcbb4384077256a5a33b3fc070
Kumar McMillan <kumar.mcmillan@gmail.com>
2014-02-13 11:42:59 -0600

    Made buckets private for easier ops management

--- apk_signer/settings/sites/dev/settings_base.py
+++ apk_signer/settings/sites/dev/settings_base.py
@@ -19,5 +19,5 @@ HAWK_CREDENTIALS['apk-factory']['key'] = private.HAWK_APK_FACTORY_KEY
 AWS_ACCESS_KEY = private.AWS_ACCESS_KEY
 AWS_SECRET_KEY = private.AWS_SECRET_KEY
 
-S3_APK_BUCKET = 'mozilla-apk-cache-dev'
-S3_KEY_BUCKET = 'mozilla-apk-keys-dev'
+S3_APK_BUCKET = private.S3_APK_BUCKET
+S3_KEY_BUCKET = private.S3_KEY_BUCKET
